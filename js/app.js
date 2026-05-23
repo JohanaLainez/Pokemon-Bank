f10758cfbf622fe781b865ccb6c921e422002be9
Smaki090 <mariasamour090@gmail.com>
2026-05-23 10:05:58 -0600

    se agrego una funcion ppara generar un comprobante en pdf junto con un diseño

--- js/app.js
+++ js/app.js
@@ -43,4 +43,48 @@ function saveBalance(balance) {
     localStorage.setItem(BALANCE_KEY, balance);
 }
 
+function generarComprobantePDF(tipo, monto, detalle) {
+        if (!window.jspdf) {
+        console.error("Error: La librería jsPDF no está cargada en este archivo HTML.");
+        return;
+    }
+
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF();
+
+    const user = getUser(); 
+    const saldoActual = getBalance();
+
+    doc.setFontSize(22);
+    doc.setTextColor(42, 109, 181); 
+    doc.setFont('helvetica', 'bold'); 
+    doc.text("POKEMON BANK ATM", 105, 25, null, null, "center");
+
+    doc.setFont('helvetica', 'normal');
+    doc.setFontSize(12);
+    doc.setTextColor(0, 0, 0);
+    doc.text("----------------------------------------------------------------", 105, 35, null, null, "center");
+
+    doc.text(`Fecha y Hora: ${new Date().toLocaleString()}`, 20, 45);
+
+    doc.text(`Cliente: ${user.nombre}`, 20, 55);
+    doc.text(`N° Cuenta: ${user.cuenta}`, 20, 65);
+
+    doc.text("----------------------------------------------------------------", 105, 75, null, null, "center");
+    
+    doc.setFont('helvetica', 'normal');
+    doc.setFontSize(14);
+    doc.text(`Transacción: ${tipo}`, 20, 85);
+    doc.text(`Detalle: ${detalle}`, 20, 95);
+    
+    doc.setFont('helvetica', 'normal');
+    doc.setFont(undefined, 'bold');
+    doc.text(`Monto: $${Number(monto).toFixed(2)}`, 20, 110);
+    doc.text(`Saldo Disponible: $${Number(saldoActual).toFixed(2)}`, 20, 120);
+
+    
+    doc.save(`Ticket_${tipo.replace(/\s+/g, '_')}_${Date.now()}.pdf`);
+    
+}
+
 iniciarDatos();
